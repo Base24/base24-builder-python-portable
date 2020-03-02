d90d0946f0af6ea8ce07092b7e97110912afaf2b
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2020-03-02 21:39:24 +0000

    minor improvements

--- dropin.py
+++ dropin.py
@@ -4,29 +4,31 @@ import sys
 import shutil
 import asyncio
 
+GIT_CLONE_PATH = "base24-builder-python-portable"
+
 # clone base24-builder-python-portable
 async def git_clone():
 	proc_env = os.environ.copy()
 	proc_env["GIT_TERMINAL_PROMPT"] = "0"
-	path = "base24-python-portable"
+
 	git_proc = await asyncio.create_subprocess_exec(
-		"git", "clone", "https://github.com/Base24/base24-builder-python-portable", path, stderr=asyncio.subprocess.PIPE, env=proc_env
+		"git", "clone", "https://github.com/Base24/base24-builder-python-portable", GIT_CLONE_PATH, stderr=asyncio.subprocess.PIPE, env=proc_env
 	)
-	_stdout, stderr = await git_proc.communicate()
+	_stdout, _stderr = await git_proc.communicate()
 	if git_proc.returncode != 0:
 		# remove created directory if it's empty
 		try:
-			os.rmdir(path)
+			os.rmdir(GIT_CLONE_PATH)
 		except OSError:
 			pass
 
 # copy base24_builder/* and base24
 def copy_dropin():
-	builder_dir = os.path.join(os.getcwd(), "base24-python-portable", "base24_builder")
+	builder_dir = os.path.join(os.getcwd(), GIT_CLONE_PATH, "base24_builder")
 	os.makedirs(os.path.join(os.getcwd(), "base24_builder"), exist_ok=True)
 	for item in os.listdir(builder_dir):
 		shutil.copy2(os.path.join(builder_dir, item), os.path.join(os.getcwd(),"base24_builder", item))
-	shutil.copy2(os.path.join(os.getcwd(), "base24-python-portable", "base24.py"), os.path.join(os.getcwd(), "base24.py"))
+	shutil.copy2(os.path.join(os.getcwd(), GIT_CLONE_PATH, "base24.py"), os.path.join(os.getcwd(), "base24.py"))
 
 asyncio.get_event_loop().run_until_complete(git_clone())
 copy_dropin()
